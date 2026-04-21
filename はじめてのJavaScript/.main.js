ec1ad6d19c1bf78fc89f7fd115ce5136d81e353a
masa <b77yellow@gmail.com>
2026-04-21 20:59:47 +0900

    基礎

---
+++ はじめてのJavaScript/.main.js
@@ -0,0 +1,22 @@
+// 厳格モード
+'use strict';
+
+let price = 150;
+let rate = 1.1
+
+console.log(price * 10);
+price += 1;
+console.log(price * 10 * rate);
+
+console.log('I\'s');
+console.log("I's");
+
+const fname = 'Taro';
+const Lname = 'Yamada';
+
+console.log(fname + Lname);
+console.log(`aaaa ${fname}`)
+
+
+const n = prompt('what nmber?')
+console.log(Number(n) + 10);
