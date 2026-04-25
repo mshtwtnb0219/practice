b5fb55b4653c7f30d388b88efc92d4bba4aa7d09
masa <b77yellow@gmail.com>
2026-04-25 17:57:45 +0900

    javascript 基礎

--- はじめてのJavaScript/.main.js
+++ はじめてのJavaScript/.main.js
@@ -1,22 +1,114 @@
 // 厳格モード
-'use strict';
+"use strict";
 
-let price = 150;
-let rate = 1.1
+// let price = 150;
+// let rate = 1.1;
 
-console.log(price * 10);
-price += 1;
-console.log(price * 10 * rate);
+// console.log(price * 10);
+// price += 1;
+// console.log(price * 10 * rate);
 
-console.log('I\'s');
-console.log("I's");
+// console.log("I's");
+// console.log("I's");
 
-const fname = 'Taro';
-const Lname = 'Yamada';
+// const fname = "Taro";
+// const Lname = "Yamada";
 
-console.log(fname + Lname);
-console.log(`aaaa ${fname}`)
+// console.log(fname + Lname);
+// console.log(`aaaa ${fname}`);
+
+// const n = prompt('what nmber?')
+// console.log(Number(n) + 10);
+
+// 条件分岐
+// const socre = Number(prompt("waht score?"));
+
+// if (socre >= 90) {
+//   console.log("good");
+// } else if (socre >= 70) {
+//   console.log("good1");
+// }
+
+// switch
+// const color = prompt('color?');
+
+// switch(color) {
+//     case 'red':
+//     case 'red1':
+//         console.log('red');
+//         break;
+//     case 'orange':
+//         console.log('orage');
+//         break;
+//     default:
+//         console.log('bad');
+//         break;
+
+// }
+
+// for
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${i}: Hello`);
+// }
+
+// const rate1 = 1.1;
+
+// for (let price = 150; price <= 160; price++) {
+//   console.log(`Price${price}`);
+//   for (let amout = 120; amout <= 140; amout += 10) {
+//     console.log(price * amout * rate1);
+//   }
+// }
+
+// while
+// let command = Number(prompt('???'));
+
+// while (command !== 0) {
+//     console.log(command);
+//     command = Number(prompt('???'));
+// }
+
+// do while
+// let command;
+// do {
+//   command = Number(prompt("?????"));
+//   if (command === 0) {
+//     console.log("exit");
+//   } else {
+//     console.log(command);
+//   }
+// } while (command !== 0);
 
 
-const n = prompt('what nmber?')
-console.log(Number(n) + 10);
+// let amout = 100;
+
+// for (let yaer = 1; yaer <= 30; yaer++) {
+//     amout *=  1.05;
+//     if (amout < 200) {
+//         continue;
+//     }
+//     if (amout > 300) {
+//         break;
+//     }
+//     console.log(`${yaer} : ${amout}`);
+
+// }
+
+// 三項演算子
+// const score = Number(prompt('what???'));
+// const result = score > 80 ? 'good' : 'bat';
+// console.log(result);
+
+
+// 論理演算子
+// const english = Number(prompt('english'));
+// const math = Number(prompt('math'));
+// if ( english >= 80 && math >= 80) {
+//     console.log('A');
+// } else {
+//     console.log('b');
+// }
+
+
+let num = 10;
+console.log(num);
---
+++ はじめてのJavaScript/.sub.js
@@ -0,0 +1,6 @@
+"use strict";
+
+{
+  let num = 20;
+  console.log(num);
+}
