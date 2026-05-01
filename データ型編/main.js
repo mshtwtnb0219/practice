2f867e2c9673336d41604cbfa2af1c193c86b88b
masa <b77yellow@gmail.com>
2026-05-01 14:08:55 +0900

    データ型

---
+++ データ型編/main.js
@@ -0,0 +1,49 @@
+"use strct";
+
+{
+  // 真偽値
+//   let flg = true;
+//   console.log(flg); // true
+
+//   const pElement = document.querySelector("p");
+//   const buttonElement = document.querySelector("button");
+
+//   buttonElement.addEventListener("click", () => {
+//     if (confirm("真偽値を切り替えますか？") === false) {
+//       if (flg === true) {
+//         pElement.textContent = "真偽値はfalseです。";
+//         flg = false;
+//       } else {
+//         pElement.textContent = "真偽値はtrueです。";
+//         flg = true;
+//       }
+//     }
+//   });
+
+    // const name = prompt("名前を入力してください。");
+    // // if (name !== '') {
+    // if (name) {
+    //     console.log(`こんにちは、${name}さん！`);
+    // } else {
+    //     console.log("こんにちは！");
+    // }
+
+    // let x;
+    // console.log(x); // undefined
+    // const scores = {math: 80, english: 90, science: null};
+    // if (scores.history === undefined) {
+    //     console.log("歴史の点数はありません。");
+    // }
+
+
+
+    // 
+    console.log(typeof 5); // false       
+    console.log(typeof 'hello'); // false       
+    console.log(typeof [1,2]); // false       
+    console.log(typeof {name: 'Alice'}); // false       
+    console.log(typeof true); // false       
+    console.log(typeof undefined); // false       
+    console.log(typeof null); // false     // false       
+
+}
